9e32ac8879e90ca5c91ebf764be52f2b3994234f
AntzUhl <1325200471@qq.com>
2020-06-08 22:54:26 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 2233-zenra-chrome-extension
+
+哔站直播区2233娘全lo谷歌插件。
+
+我只是做了小小的谷歌插件外壳，骚气代码作者在下面：
+
+[https://github.com/journey-ad](https://github.com/journey-ad)
+
+[https://gist.github.com/journey-ad/5d3021fd40db75de2df827b876ef5b7e](https://gist.github.com/journey-ad/5d3021fd40db75de2df827b876ef5b7e)
---
+++ js/base.js
@@ -0,0 +1,8 @@
+window.onload=function (){
+	jsPath = 'js/2233zenra.js';
+	var temp = document.createElement('script');
+	temp.setAttribute('type', 'text/javascript');
+	// 获得的地址类似：chrome-extension://ihcokhadfjfchaeagdoclpnjdiokfakg/js/inject.js
+	temp.src = chrome.extension.getURL(jsPath);
+	document.head.appendChild(temp);
+}
